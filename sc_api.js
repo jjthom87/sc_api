6bb42f0ebfd9f2f40dcb9e7e46b2cd7724c91a67
Jared Thomas <jjthom87@yahoo.com>
2017-10-09 17:40:51 -0400

    figured out api

--- sc_api.js
+++ sc_api.js
@@ -4,10 +4,6 @@ $(document).ready(function(){
 	  client_id: 'fd4e76fc67798bfa742089ed619084a6',
 	});
 
-	// SC.get('/users/laintr/tracks').then(function(tracks){
-	// 	console.log(tracks)
-	// })
-
 	var input = $('<input>',{
 		id: 'track-input',
 		type: 'text',
@@ -22,47 +18,62 @@ $(document).ready(function(){
 	$('div').eq(0).append(input).append('<br>').append(button).append('<br>')
 
 	var random;
+	var songDiv = $('<div>');
+	songDiv.addClass('song-div');
+	var songOl = $('<ol>');
+	songOl.addClass('song-ol');
 	$(document).on('click', '#search-button', function(){
+		$('.song-div').remove();
 		SC.get('/tracks',{
 			q: $('#track-input').val()
 		}).then(function(response){
-			random = Math.floor(Math.random() * response.length);
-			SC.stream("/tracks/" + response[random].id).then(function(player) {
-				player.play();
-			});
+			var str = "<ol>";
+			for(var i = 0; i < response.length; i++){
+				str += "<li><button class='song-button' data-id=" + response[i].id + ">" + response[i].title + "</button></li>"
+			}
+			str += "</ol>"
+			songDiv.html(str);
+			$('div').eq(0).append(songDiv)
 		})
 	})
 
-	var playButton = $('<button>');
-	playButton.addClass('play-button');
-	playButton.text("Play")
-	$('div').eq(0).append(playButton)
-
-	var num = 0
-	$(document).on('click', '.play-button', function(){
-		SC.get('/tracks',{
-			q: "heart"
-		}).then(function(response){
-			SC.stream("/tracks/" + response[num].id).then(function(player) {
-				player.play();
-			});
-		})
+	$(document).on('click', '.song-button', function(){
+		var id = $(this).data('id');
+		SC.stream("/tracks/" + id).then(function(player) {
+			player.play();
+		});
 	})
 
-	var nextButton = $('<button>');
-	nextButton.addClass('next-button');
-	nextButton.text('Next')
-	$('div').eq(0).append(nextButton);
+	// var playButton = $('<button>');
+	// playButton.addClass('play-button');
+	// playButton.text("Play")
+	// $('div').eq(0).append(playButton)
 
-	$(document).on('click', '.next-button', function(){
-		num++;
-		SC.get('/tracks',{
-			q: "heart"
-		}).then(function(response){
-			SC.stream("/tracks/" + response[num].id).then(function(player) {
-				player.play();
-			});
-		})
-	})
+	// var num = 0
+	// $(document).on('click', '.play-button', function(){
+	// 	SC.get('/tracks',{
+	// 		q: "heart"
+	// 	}).then(function(response){
+	// 		SC.stream("/tracks/" + response[num].id).then(function(player) {
+	// 			player.play();
+	// 		});
+	// 	})
+	// })
+
+	// var nextButton = $('<button>');
+	// nextButton.addClass('next-button');
+	// nextButton.text('Next')
+	// $('div').eq(0).append(nextButton);
+
+	// $(document).on('click', '.next-button', function(){
+	// 	num++;
+	// 	SC.get('/tracks',{
+	// 		q: "heart"
+	// 	}).then(function(response){
+	// 		SC.stream("/tracks/" + response[num].id).then(function(player) {
+	// 			player.play();
+	// 		});
+	// 	})
+	// })
 
 })
